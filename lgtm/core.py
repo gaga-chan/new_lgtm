4c5940bfc19b0cf94cef9c2ea3fa03cfc50b7ba7
Akira Memasen <hisanka@hisankanoMacBook-ea.local>
2020-12-24 07:23:01 +0900

    test

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,16 +1,20 @@
 # coding = UTF-8
+from lgtm.drawer import save_with_message
+from lgtm.image_source import get_image
 
 import click
 # 画像のソース情報とメッセージを受け取れるようにする
 @click.command()
-@click.option('--message', '-m', default='LGTM',
+@click.option('--message', '-m', default='@AKIRA-MEMASEN(^_^;)',
                 show_default=True, help='画像に乗せる文字列')
 @click.argument('keyword')
 def cli(keyword, message):
     """LGTM画像生成ツール"""
     lgtm(keyword, message)
-    click.echo('lgtm') # 動作確認用
+    #click.echo('lgtm') # 動作確認用
 
 def lgtm(keyword, message):
     # ここにロジックを追加していく
-    pass
+    with get_image(keyword) as fp:
+        save_with_message(fp, message)
+    #pass
--- lgtm/image_source.py
+++ lgtm/image_source.py
@@ -1,5 +1,6 @@
 import requests
-import ByteIO
+from io import BytesIO
+from pathlib import Path
 
 class LocalImage:
     """ファイルから画像を取得する"""
@@ -21,6 +22,37 @@ class RemoteImage:
     def get_image(self):
         data = requests.get(self._url)
         print(f'{type(data)=}')
-        print(f'{type(ByteIO(data.context))=}')
         # バイトデータをファイルオブジェクトに変換
-        return BytesIO(data.context)
+        return BytesIO(data.content)
+
+class _LoremFlickr(RemoteImage):
+    """キーワード検索で画像を取得する"""
+    LOREM_FLICKR_URL = 'https://loremflickr.com'
+    WIDTH = 800
+    HEIGHT = 600
+
+    def __init__(self, keyword):
+        super().__init__(self._build_url(keyword))
+
+    def _build_url(self, keyword):
+        return (f'{self.LOREM_FLICKR_URL}/'
+                f'{self.WIDTH}/{self.HEIGHT}/{keyword}')
+
+KeywordImage = _LoremFlickr
+print(f'{KeywordImage=}')
+print(f'{type(KeywordImage)=}')
+
+# コンストラクタとして利用するため
+# 単語を大文字始まりにしてクラスのように見せる
+def ImageSource(keyword):
+    """最適なイメージソースクラスを返す"""
+    if keyword.startswith(('http://', 'https://')):
+        return RemoteImage(keyword)
+    elif Path(keyword).exists():
+        return LocalImage(keyword)
+    else:
+        return KeywordImage(keyword)
+
+def get_image(keyword):
+    """画像のファイルオブジェクトを返す"""
+    return ImageSource(keyword).get_image()
