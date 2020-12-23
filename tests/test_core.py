4ae3c56a509d413a86dd8221c8d6bc505a50db52
Akira Memasen <hisanka@hisankanoMacBook-ea.local>
2020-12-24 05:10:32 +0900

    lgtm/core.pyの変更に伴うテストの修正

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,10 @@
 import unittest
 
 class LgtmTest(unittest.TestCase):
+    #def test_lgtm(self):
+    #    from lgtm.core import lgtm
+    #    self.assertIsNone(lgtm())
+        
     def test_lgtm(self):
         from lgtm.core import lgtm
-        self.assertIsNone(lgtm())
-        
+        self.assertIsNone(lgtm('./python.jpeg', 'LGTM'))
